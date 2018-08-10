b7bde396fc5daae3cdc3a743b4fe9777fc0d4333
u201510106 (Riveros Herrada, Grete Xiomara) <u201510106@upc.edu.pe>
2018-08-10 06:45:58 +0000

    [Finishes #159679658] Terminado

--- C_Datos/Database.cs
+++ C_Datos/Database.cs
@@ -7,7 +7,7 @@ using System.Data.SqlClient;
 using System.Data;
 namespace C_Datos
 {
-    class Database
+    public class Database
     {
         private SqlConnection conn;
         public SqlConnection ConectarBD()
