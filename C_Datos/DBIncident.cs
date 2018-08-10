004417975523843ecfaf61179c4f26ef7bc851ef
u201510106 (Riveros Herrada, Grete Xiomara) <u201510106@upc.edu.pe>
2018-08-10 07:33:01 +0000

    BDIncident: Estructura de conexion

--- C_Datos/DBIncident.cs
+++ C_Datos/DBIncident.cs
@@ -5,10 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using C_Negocio;
 namespace C_Datos
 {
     public class DBIncident
     {
-       
+        Database DB = new Database();
+        SqlConnection cn;
+        public void IngresarIncident(nIncident c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
+        public void ModificarIncident(nIncident c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
+        public void EliminarIncident(String c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
