4314eae2929dd5c9d5cfb138939102f084861daf
u201510106 (Riveros Herrada, Grete Xiomara) <u201510106@upc.edu.pe>
2018-08-10 07:33:51 +0000

    BDVehicle: Estructura de conexion

--- C_Datos/DBVehicle.cs
+++ C_Datos/DBVehicle.cs
@@ -5,11 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using C_Negocio;
 
 namespace C_Datos
 {
     public class DBVehicle
     {
-        
+        Database DB = new Database();
+        SqlConnection cn;
+        public void IngresarVehicle(nVehicle c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
+        public void ModificarVehicle(nVehicle c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
+        public void EliminarVehicle(String c)
+        {
+            cn = DB.ConectarBD();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("", c);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
